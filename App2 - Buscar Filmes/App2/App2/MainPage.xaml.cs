58e3ec0c03d2ffe0c83d6ef55d17d3b680668949
luissantosbcc <luis.santos.bcc@gmail.com>
2020-06-25 12:35:03 -0300

    Criação do app2 - Buscar Filmes

---
+++ App2 - Buscar Filmes/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App2.Services;
+using App2.Services.Models;
+
+namespace App2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarFilme;
+        }
+
+        private void BuscarFilme(object sender, EventArgs args)
+        {
+            string filme = FILME.Text.Trim();
+
+                try
+                {
+                    Movies end = Search.BuscarEndereco(filme);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Titulo: {0} \n" +
+                                                          "Ano: {1} \n" +
+                                                          "Released: {2} \n" +
+                                                          "Plot: {3} \n" +
+                                                          "Genre: {4} \n" +
+                                                          "Director: {5} \n" +
+                                                          "Writer: {6} \n" +
+                                                          "Actors: {7} \n" +
+                                                          "Language: {8} \n" +
+                                                          "Country: {9} \n" +
+                                                          "IMDB Rating: {10} \n"
+                                                         , end.Title, 
+                                                           end.Year, 
+                                                           end.Released, 
+                                                           end.Plot,
+                                                           end.Genre,
+                                                           end.Director,
+                                                           end.Writer,
+                                                           end.Actors,
+                                                           end.Language,
+                                                           end.Country,
+                                                           end.imdbRating);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O filme não foi encontrado: " + filme, "OK");
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }            
+        }
+    }
+}
---
+++ App2 - Buscar Filmes/App2/App2/Services/Search.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App2.Services.Models;
+using Newtonsoft.Json;
+
+namespace App2.Services
+{
+    public class Search
+    {
+        private static string EnderecoURL = "http://www.omdbapi.com/?t={0}&apikey=2d4ef165";
+
+        public static Movies BuscarEndereco(String movie)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, movie);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Movies end = JsonConvert.DeserializeObject<Movies>(Conteudo);
+
+            if (end.Response == "False") return null;
+
+            return end;
+        }
+    }
+}
