7bea9f7e60ee8101e60dd2f102dcf215302c87fc
luissantosbcc <luis.santos.bcc@gmail.com>
2020-06-25 09:33:59 -0300

    App01 - Buscar CEP - Concluído

---
+++ App1 - Buscar CEP/App1/App1/Services/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1.Services.Models;
+using Newtonsoft.Json;
+
+namespace App1.Services
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(String cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
